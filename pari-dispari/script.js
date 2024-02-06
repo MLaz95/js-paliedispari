b70d8a3e375e2f309481536f23cddba3e0209af0
marco <lazzari.marco.ml@gmail.com>
2024-02-06 16:29:10 +0100

    aggiunta validazione dati

--- pari-dispari/script.js
+++ pari-dispari/script.js
@@ -1,10 +1,3 @@
-// user chooses odds or evens
-// user picks a number between 1 and 5
-// computer generates a number in the same range
-// sum the two values
-// determine if the result is odd or even
-// tell the user if they guessed right
-
 // generates random number between min of b and max of a
 function random (a,b){
     rnd = Math.floor(Math.random() * a + b);
@@ -20,18 +13,39 @@ function oddOrEven (x){
     }
 }
 
-
 const startButton = document.querySelector('#play');
 
 
 startButton.addEventListener('click',
     function() {
-        const userGuess = prompt('Choose odd or even');
-        const userNumber = Number(prompt('Pick a number from 1 to 5'));
+        let userGuess;
+        let userNumber;
+
+        // user chooses odd or even
+        do{
+            
+            userGuess = prompt('Choose odd or even');
+            if(userGuess != 'odd' && userGuess != "even"){
+                alert('invalid input, try again')
+            }
+        } while(userGuess != 'odd' && userGuess != "even") // ensures user input valid data
+        
+
+        // user picks a number between 1 and 5
+        do{
+            userNumber = Number(prompt('Pick a number from 1 to 5'));
+            if(Number.isNaN(userNumber) || userNumber > 5 || userNumber < 1){
+                alert('invalid input, try again')
+            }
+        } while(Number.isNaN(userNumber) || userNumber > 5 || userNumber < 1) // ensures user input valid data
+        
+        // computer generates random number in the same range
         const computerNumber = random(5,1);
 
+        // sum the two values
         const total = userNumber + computerNumber;
-        
+
+        // determine winner
         if(userGuess == oddOrEven(total)){
             console.log('congrats you won!')
         } else{
